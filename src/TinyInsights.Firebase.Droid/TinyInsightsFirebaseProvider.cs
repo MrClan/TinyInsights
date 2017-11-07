e0e269e1df23666939a7828838e145ba88f8e6a9
Daniel Hindrikes <daniel.hindrikes@sogeti.se>
2017-11-07 09:00:29 +0100

    created iOS and android projects for firebase provider

---
+++ src/TinyInsights.Firebase.Droid/TinyInsightsFirebaseProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyInsights.Firebase.Droid
+{
+    public class TinyInsightsFirebaseProvider : ITinyInsights
+    {
+        public Task TrackErrorAsync(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackEventAsync(string eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackEventAsync(string eventName, Dictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackPageViewAsync(string viewname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackPageViewAsync(string viewName, Dictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
